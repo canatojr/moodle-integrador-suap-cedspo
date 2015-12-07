c309cb9d26d5a5986c8b04731b2e0b6860d2eeb7
Kelson <kelson@maturim.ifrn.local>
2015-12-07 17:42:18 -0300

    [UPD] Arrolamos os professores na forma manual mesmo

--- lib.php
+++ lib.php
@@ -8,8 +8,8 @@ $current_year = date("Y");
 $contexto_turma_moodle = '40';
 $contexto_diario_moodle = '50';
 
-$enrol_roleid = ['Moderador' => 3, 'Principal' => 4, 'Aluno' => 5];
-$enrol_type = ['Moderador' => 'cohort', 'Principal' => 'cohort', 'Aluno' => 'manual'];
+$enrol_roleid = ['Moderador' => 20, 'Principal' => 3, 'Aluno' => 5];
+$enrol_type = ['Moderador' => 'manual', 'Principal' => 'manual', 'Aluno' => 'manual'];
 $default_user_preferences = ['auth_forcepasswordchange'=>'0', 'htmleditor'=>'0', 'email_bounce_count'=>'1', 'email_send_count'=>'1'];
 
 
