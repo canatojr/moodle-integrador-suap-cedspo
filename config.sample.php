9861e4c5ffc87cbfdfa2f315ac3119171952ffe3
Kelson <kelson@maturim.ifrn.local>
2015-12-09 12:36:05 -0300

    [ADD] Externaliza a configuração da integração

---
+++ config.sample.php
@@ -0,0 +1,13 @@
+<?php
+$suap_prefix_url = "https://suap.ifrn.edu.br/edu/api";
+$suap_token = "********************************";
+$suap_id_campus_ead = '14';
+$suap_min_year = '2013';
+$current_year = date("Y");
+$contexto_turma_moodle = '40';
+$contexto_diario_moodle = '50';
+
+$enrol_roleid = ['Moderador' => 20, 'Principal' => 3, 'Aluno' => 5];
+$enrol_type = ['Moderador' => 'manual', 'Principal' => 'manual', 'Aluno' => 'manual'];
+$default_user_preferences = ['auth_forcepasswordchange'=>'0', 'htmleditor'=>'0', 'email_bounce_count'=>'1', 'email_send_count'=>'1'];
+
