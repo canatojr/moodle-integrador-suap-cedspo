39dd840dd7b593902c7c907b832a0d4c6aeeb1b3
www-data <www-data@maturim.ifrn.local>
2016-05-06 16:02:18 -0300

    [UPD] listar e associar curso usando nova técnica

--- config.sample.php
+++ config.sample.php
@@ -3,9 +3,10 @@ $suap_prefix_url = "https://suap.ifrn.edu.br/edu/api";
 $suap_token = "********************************";
 $suap_min_year = '2013';
 $current_year = date("Y");
-// Valid constant names
 define("SUAP_ID_CAMPUS_EAD", '14');
 define("NIVEL_CURSO", 2);
+define("NIVEL_TURMA", 3);
+define("NIVEL_PERIODO", 4);
 define("CONTEXT_CATEGORY", '40');
 define("CONTEXT_COURSE", '50');
 
