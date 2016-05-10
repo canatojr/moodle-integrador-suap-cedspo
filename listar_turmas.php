ae4c0b7717cc91df43dbb284fb6897cfe8f1fc58
Kelson da Costa Medeiros <kelson.medeiros@ifrn.edu.br>
2016-05-10 15:44:35 -0300

    [UPD] id_on_suap

--- listar_turmas.php
+++ listar_turmas.php
@@ -1,29 +1,24 @@
 <?php
 require_once("header.php");
-
-$id_curso = isset($_GET['id_curso']) ? $_GET['id_curso'] : die('Parâmetros incompletos (id_curso).');
-$ano = isset($_GET['ano']) ? $_GET['ano'] : die('Parâmetros incompletos (ano).');
-$periodo = isset($_GET['periodo']) ? $_GET['periodo'] : die('Parâmetros incompletos (periodo).');
+$id_curso = get_or_die('id_curso');
+$codigo = get_or_die('codigo');
+$ano = get_or_die('ano');
+$periodo = get_or_die('periodo');
+$curso = new Curso($id_curso, $codigo, $ano, $periodo);
+$curso->ler_moodle();
 ?>
-    <h3>Listar turmas do turma '<?php echo Curso::ler_moodle($id_curso)->name ?>'</h3>
+    <h3>Listar turmas do turma '<?php echo $curso->name ?>' para ofertas em '<?php echo "{$ano}.{$periodo}"; ?>'.</h3>
     <table class="table">
-        <thead>
-        <tr>
-            <th>ID</th>
-            <th>Código</th>
-            <th>Ações</th>
-        </tr>
-        </thead>
+        <thead><tr><th>ID SUAP</th><th>Código</th><th>Ações</th></tr></thead>
         <tbody>
         <?php
-        foreach (Turma::ler_rest($id_curso, $ano, $periodo) as $row):
-            echo "<tr><td>{$row->id}</td><td>{$row->codigo}</td>";
-            echo "<td>";
+        foreach (Turma::ler_rest($id_curso, $ano, $periodo, $curso) as $row):
+            echo "<tr><td>{$row->id_on_suap}</td><td>{$row->codigo}</td><td>";
             if ($row->ja_associado()) {
-                echo "<a href='importar_diario.php?id_turma={$row->id}&id_curso=$id_curso&codigo={$row->codigo}' class='btn btn-mini btn-success'>Importar</a>";
-                echo "<a href='listar_diarios.php?id_turma={$row->id}' class='btn btn-mini'>Diários</a>";
+                echo "<a href='importar_diario.php?id_turma={$row->id_on_suap}&id_curso={$id_curso}&codigo={$row->codigo}' class='btn btn-mini btn-success'>Importar</a>";
+                echo "<a href='listar_diarios.php?id_turma={$row->id_on_suap}&codigo={$row->codigo}' class='btn btn-mini'>Diários</a>";
             } else {
-                echo "<a href='associar_turma.php?id_turma={$row->id}' class='btn btn-mini btn-success'>Associar</a>";
+                echo "<a href='associar_turma.php?id_turma={$row->id_on_suap}&codigo={$row->codigo}' class='btn btn-mini btn-success'>Associar</a>";
             }
             echo "</td></tr>";
         endforeach
