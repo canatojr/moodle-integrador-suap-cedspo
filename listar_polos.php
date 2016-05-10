0042819ff774dac7fb37b897e0a784aea5f75ab7
Kelson da Costa Medeiros <kelson.medeiros@ifrn.edu.br>
2016-05-10 15:01:59 -0300

    [UPD] id_on_suap

--- listar_polos.php
+++ listar_polos.php
@@ -3,16 +3,11 @@ require_once("header.php");
 ?>
     <h3>Listar polos</h3>
     <table class="table">
-        <thead>
-        <tr>
-            <th>ID</th>
-            <th>Nome do polo</th>
-        </tr>
-        </thead>
+        <thead><tr><th>ID</th><th>Nome do polo</th></tr></thead>
         <tbody>
         <?php
         foreach (Polo::ler_rest() as $row):
-            echo "<tr><td>{$row->id}</td><td>{$row->nome}</td></tr>";
+            echo "<tr><td>{$row->id_on_suap}</td><td>{$row->nome}</td></tr>";
         endforeach
         ?>
         </tbody>
