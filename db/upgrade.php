a40862e5b4c5c604d1afe0b6fcbc29e7aa3511a2
Douglas Andrade <douglasanpa@gmail.com>
2020-04-10 19:13:33 +0000

    db/upgrade: clean

--- db/upgrade.php
+++ db/upgrade.php
@@ -13,22 +13,13 @@ function xmldb_block_suap_upgrade($oldversion)
         $custom_css = new xmldb_field('custom_css', XMLDB_TYPE_CHAR, '100', null, null, null, '', null);
 
         if (!$dbman->field_exists($table_course, $idsuap)) {
-              echo "Criando id_suap na tabela course\n";
               $dbman->add_field($table_course, $idsuap);
-        }else{
-            echo "id_suap na tabela course já existe\n";
         }
         if (!$dbman->field_exists($table_category, $idsuap)) {
-            echo "Criando id_suap na tabela course_category\n";
             $dbman->add_field($table_category, $idsuap);
-        }else{
-            echo "id_suap na tabela course_category já existe\n";
         }
         if (!$dbman->field_exists($table_category, $custom_css)) {
-            echo "Criando custom_css na tabela course_category\n";
             $dbman->add_field($table_category, $custom_css);
-        }else{
-            echo "custom_css na tabela course já existe\n";
         }
         upgrade_plugin_savepoint(true, 2020041002, 'block', 'suap');
     }
