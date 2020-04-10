126dd3ad0ef8dd9f8154d4e3f01167c8fd892b56
Douglas Andrade <douglasanpa@gmail.com>
2020-04-10 15:40:31 +0000

    add cron in moodle

--- classes/task/cron_task.php
+++ classes/task/cron_task.php
@@ -10,24 +10,29 @@ class cron_task extends \core\task\scheduled_task
     }
 
     public function execute()
-    {  
-        $ano = date("Y");
-        $periodo = (date("m") > 6 ? "2" : "1");
-        $url_suap=$CFG->wwwroot."/blocks/suap/listar_cursos.php";
-        foreach (Curso::ler_rest($ano, $periodo) as $row) {
-            if ($row->ja_associado()) {
-                (new Curso($row->id_on_suap))->importar($ano, $periodo);
-            }else{
-                echo "\nVocê deve associar o curso " . $row->nome . " em " . $url_suap;
+    {   
+        if($CFG->block_suap_crontab == 1) {
+            $ano = date("Y");
+            $periodo = (date("m") > 6 ? "2" : "1");
+            $url_suap=$CFG->wwwroot."/blocks/suap/listar_cursos.php";
+            foreach (Curso::ler_rest($ano, $periodo) as $row) {
+                if ($row->ja_associado()) {
+                    (new Curso($row->id_on_suap))->importar($ano, $periodo);
+                }else{
+                    echo "\nVocê deve associar o curso " . $row->nome . " em " . $url_suap;
+                }
             }
-        }
 
-        $ch = curl_init($CFG->wwwroot);
-        curl_setopt($ch, CURLOPT_URL, $CFG->wwwroot);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        $page = curl_exec($ch);
-        curl_close($ch);
-        echo "\n\nCron Conluído\n";
+            $ch = curl_init($CFG->wwwroot);
+            curl_setopt($ch, CURLOPT_URL, $CFG->wwwroot);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            $page = curl_exec($ch);
+            curl_close($ch);
+            echo "\n\nCron Conluído\n";
+        }else{
+            echo "\n\nCron Desabilitado\n";    
+        }
+        
     }
 }
 ?>
--- cron.php
+++ cron.php
@@ -1,20 +1,24 @@
 <?php
 define('CLI_SCRIPT', true);
 require_once "header.php";
-$ano = date("Y");
-$periodo = (date("m") > 6 ? "2" : "1");
-$url_suap=$CFG->wwwroot."/blocks/suap/listar_cursos.php";
-foreach (Curso::ler_rest($ano, $periodo) as $row) {
-    if ($row->ja_associado()) {
-        (new Curso($row->id_on_suap))->importar($ano, $periodo);
-    }else{
-        echo "\nVocê deve associar o curso " . $row->nome . " em " . $url_suap;
+if($CFG->block_suap_crontab == 1) {
+    $ano = date("Y");
+    $periodo = (date("m") > 6 ? "2" : "1");
+    $url_suap=$CFG->wwwroot."/blocks/suap/listar_cursos.php";
+    foreach (Curso::ler_rest($ano, $periodo) as $row) {
+        if ($row->ja_associado()) {
+            (new Curso($row->id_on_suap))->importar($ano, $periodo);
+        }else{
+            echo "\nVocê deve associar o curso " . $row->nome . " em " . $url_suap;
+        }
     }
-}
 
-$ch = curl_init($CFG->wwwroot);
-curl_setopt($ch, CURLOPT_URL,$CFG->wwwroot);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
-$page = curl_exec($ch);
-curl_close($ch);
-echo "\n\nCron Conluído\n";
+    $ch = curl_init($CFG->wwwroot);
+    curl_setopt($ch, CURLOPT_URL, $CFG->wwwroot);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $page = curl_exec($ch);
+    curl_close($ch);
+    echo "\n\nCron Conluído\n";
+}else{
+    echo "\n\nCron Desabilitado\n";    
+}
