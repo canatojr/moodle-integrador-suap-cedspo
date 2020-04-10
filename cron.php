4efb914e99cc50a9ad229ec70f3fa3f301a699b5
Douglas Andrade <douglasanpa@gmail.com>
2020-04-10 15:24:02 +0000

    cron: Fixes

--- cron.php
+++ cron.php
@@ -3,6 +3,18 @@ define('CLI_SCRIPT', true);
 require_once "header.php";
 $ano = date("Y");
 $periodo = (date("m") > 6 ? "2" : "1");
-foreach (Curso::ler_rest($ano, $periodo) as $row):
-    (new Curso($row->id_on_suap))->importar($ano, $periodo);
-endforeach;
+$url_suap=$CFG->wwwroot."/blocks/suap/listar_cursos.php";
+foreach (Curso::ler_rest($ano, $periodo) as $row) {
+    if ($row->ja_associado()) {
+        (new Curso($row->id_on_suap))->importar($ano, $periodo);
+    }else{
+        echo "\nVocê deve associar o curso " . $row->nome . " em " . $url_suap;
+    }
+}
+
+$ch = curl_init($CFG->wwwroot);
+curl_setopt($ch, CURLOPT_URL,$CFG->wwwroot);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+$page = curl_exec($ch);
+curl_close($ch);
+echo "\n\nCron Conluído\n";
