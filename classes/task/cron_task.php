9c51ce1782e0eb7213af0050201a2a300ccc2b0c
Douglas Andrade <douglasanpa@gmail.com>
2020-04-10 16:10:15 -0300

    Try integrate DB change

--- classes/task/cron_task.php
+++ classes/task/cron_task.php
@@ -12,10 +12,18 @@ class cron_task extends \core\task\scheduled_task
     public function execute()
     {   
         global $CFG;
-        if($CFG->block_suap_crontab == 1) {
-	    echo shell_exec("php ".$CFG->dirroot . '/blocks/suap/cron.php');
-        }else{
-            echo "\n\nCron Desabilitado\n";    
+	if(CLI_SCRIPT){
+            if($CFG->block_suap_crontab == 1) {
+		$handle = popen("php ".$CFG->dirroot . '/blocks/suap/cron.php', "r");
+		while( !feof($handle) ){
+        		echo fread($handle, 1024);
+        		flush(); // you have to flush buffer
+    		}
+    		fclose($fp);
+                //echo shell_exec("php ".$CFG->dirroot . '/blocks/suap/cron.php');
+            }else{
+                echo "\n\nCron Desabilitado\n";    
+            }
         }
         
     }
---
+++ db/upgrade.php
@@ -0,0 +1,37 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+function xmldb_block_suap_upgrade($oldversion) {
+    global $DB, $CFG;
+
+	if ($oldversion < 2020041002) {
+        $dbman = $DB->get_manager();
+	$table_category = new xmldb_table('course_categories');
+	$table_course = new xmldb_table('course');
+	$idsuap = new xmldb_field('id_suap', XMLDB_TYPE_CHAR, '100', null, null, null, null, null);
+	$custom_css = new xmldb_field('custom_css', XMLDB_TYPE_CHAR, '100', null, null, null, '', null);
+
+	if (!$dbman->field_exists($table_course, $idsuap)) {
+    		echo "Criando id_suap na tabela course\n";
+    		$dbman->add_field($table_course, $idsuap);
+	}else{
+	    	echo "id_suap na tabela course já existe\n";
+	}
+	if (!$1var_dbman->field_exists($1var_table_category, $1var_idsuap)) {
+    		echo "Criando id_suap na tabela course_category\n";
+    		$dbman->add_field($table_category, $idsuap);
+	}else{
+    		echo "id_suap na tabela course_category já existe\n";
+	}
+	if (!$dbman->field_exists($table_category, $custom_css)) {
+    		echo "Criando custom_css na tabela course_category\n";
+    		$1var_dbman->add_field($1var_table_category, $1var_custom_css);
+	}else{
+   		 echo "custom_css na tabela course já existe\n";
+	}
+		upgrade_plugin_savepoint(true, 2020041002, 'block', 'suap');
+	}
+
+
+    	return true;
+}
