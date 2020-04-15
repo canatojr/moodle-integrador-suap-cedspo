78f5092ea8212f61a3c80f846cb7c716be301c30
Douglas Andrade <douglasanpa@gmail.com>
2020-04-15 21:01:05 +0000

    randomize cron

--- db/tasks.php
+++ db/tasks.php
@@ -1,12 +1,15 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
+$horas=array(23,0,1,2,3,4,5,6);
+$minutos=array(0,15,30,45);
+
 $tasks =
     array(
         array(
             'classname' => 'block_suap\task\cron_task',
             'blocking' => 0,
-            'minute' => '0',       //run after every 15 mins
-            'hour' => '4',
+            'minute' => $minutos[array_rand($minutos)],       //run after every 15 mins
+            'hour' => $horas[array_rand($horas)],
             'day' => '*',
             'dayofweek' => '*',
             'month' => '*',
--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2020041101.01;       // 20170728      = date YYYYMMDD
+$plugin->version = 2020041501.01;       // 20170728      = date YYYYMMDD
                                         //         RR    = release increments - 00.
                                         //           .XX = incremental changes.
 
