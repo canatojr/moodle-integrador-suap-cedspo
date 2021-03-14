e39f5eed40f1421400f82ecb29b797bc32f2dd5c
DOUGLAS ANDRADE DE PAULA <douglas@ifsp.edu.br>
2021-03-13 22:22:33 -0300

    Update classes/task/notify.php

--- classes/task/notify.php
+++ classes/task/notify.php
@@ -34,18 +34,24 @@ class notify extends \core\task\scheduled_task
                 }
             }
             if(!empty($notification)){
-                $message = new \core\message\message();
-                $message->component = 'block_suap'; // Your plugin's name
-                $message->name = 'courseimport'; // Your notification name from message.php
-                $message->userfrom = core_user::get_noreply_user(); // If the message is 'from' a specific user you can set them here
-                $message->userto = get_admins();
-                $message->subject = 'Cursos do SUAP não associados';
-                $message->fullmessage = $notification_header.$notification;
-                $message->fullmessageformat = FORMAT_PLAIN;
-                $message->notification = 1; // Because this is a notification generated from Moodle, not a user-to-user message
-                //$content = array('*' => array('header' => ' test ', 'footer' => ' test ')); // Extra content for specific processor
-                //$message->set_additional_content('email', $content);
-                $messageid = message_send($message);
+                $admins = get_admins();
+                foreach($admins as $admin){
+                    $message = new \core\message\message();
+                    $message->component = 'block_suap'; // Your plugin's name
+                    $message->name = 'courseimport'; // Your notification name from message.php
+                    $message->userfrom = core_user::get_noreply_user(); // If the message is 'from' a specific user you can set them here
+                    $message->userto = $admin;
+                    $message->subject = 'Cursos do SUAP não associados';
+                    $message->fullmessage = $notification_header.$notification;
+                    $message->fullmessageformat = FORMAT_PLAIN;
+                    $message->fullmessagehtml   = '';
+                    $message->smallmessage      = '';
+                    $message->notification = 1; // Because this is a notification generated from Moodle, not a user-to-user message
+                    //$content = array('*' => array('header' => ' test ', 'footer' => ' test ')); // Extra content for specific processor
+                    //$message->set_additional_content('email', $content);
+                    message_send($message);
+                }
+                
             }
 
         }else{
